2ae2ebf4fd099cf1184bda4d7e6e710e243fbd5d
tushar gandhi <tugandhi@mafcarrefour.com>
2021-06-12 00:13:23 +0530

    first commit

---
+++ src/main/java/com/oms/IntuitOmsApplication.java
@@ -0,0 +1,13 @@
+package com.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitOmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntuitOmsApplication.class, args);
+	}
+
+}
